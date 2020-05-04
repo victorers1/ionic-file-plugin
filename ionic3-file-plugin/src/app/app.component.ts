771efa8d88db6b1bb7d0f8e6b6e440fe8df1bed9
Victor Emanuel <victorers2@gmail.com>
2020-05-04 10:42:10 -0300

    Fixes basepath initialization

--- ionic3-file-plugin/src/app/app.component.ts
+++ ionic3-file-plugin/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, Events } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -10,11 +10,18 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public event: Events,
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
 
+
+      this.event.publish('basepath:set');
 
       statusBar.styleDefault();
       splashScreen.hide();
--- ionic3-file-plugin/src/pages/home/home.ts
+++ ionic3-file-plugin/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { File } from '@ionic-native/file';
-import { NavController, Platform } from 'ionic-angular';
+import { NavController, Platform, Events } from 'ionic-angular';
 import { Log } from '../../models/ConsoleLog';
 
 @Component({
@@ -16,9 +16,18 @@ export class HomePage {
     public navCtrl: NavController,
     public file: File,
     public p: Platform,
+    public event: Events,
   ) {
-    this.basePath = this.file.dataDirectory;
+
     this.logs.push(new Log(`Base Path`, `basePath initialized with ${this.basePath}`))
+
+    this.event.subscribe('basepath:set', () => {
+      if (this.p.is('cordova')) {
+        this.basePath = this.file.dataDirectory;
+      } else {
+        this.logs.push(new Log(`Base Path`, `Could not set base path. Not on Cordova device.`));
+      }
+    });
   }
 
   createFolder() {
@@ -28,25 +37,30 @@ export class HomePage {
       return;
     }
 
-    this.file.checkDir(`${this.basePath}`, this.folderName)
-      .then(res => {
-        this.logs.push(new Log(`Checking Directory ${this.folderName}`, JSON.stringify(res)));
-        alert(`Pasta ${this.folderName} já existe`);
+    if (this.basePath == null) {
+      this.event.publish('basepath:set');
+      this.logs.push(new Log(`Base Path`, `basePath was null. Try again.`));
 
-      }).catch(err => {
-        this.logs.push(new Log(`Error checking folder ${this.folderName}`, JSON.stringify(err)));
-        alert(`Pasta ${this.folderName} não existe. Tentarei criá-la.`);
+    } else {
+      this.file.checkDir(`${this.basePath}`, this.folderName)
+        .then(res => {
+          this.logs.push(new Log(`Checking Directory ${this.folderName}`, JSON.stringify(res)));
+          alert(`Pasta ${this.folderName} já existe`);
 
-        this.file.createDir(`${this.basePath}`, this.folderName, true)
-          .then(res => {
-            alert(`Pasta ${this.folderName} created, enjoy.`);
-            this.logs.push(new Log(`Created Directory ${this.folderName}`, JSON.stringify(res)));
+        }).catch(err => {
+          this.logs.push(new Log(`Error checking folder ${this.folderName}`, JSON.stringify(err)));
+          alert(`Pasta ${this.folderName} não existe. Tentarei criá-la.`);
 
-          }).catch(err => {
-            this.logs.push(new Log(`Error creating folder ${this.folderName}`, JSON.stringify(err)));
-            alert(`Error creating folder ${this.folderName}: ${JSON.stringify(err)}`);
-          });
-      });
+          this.file.createDir(`${this.basePath}`, this.folderName, true)
+            .then(res => {
+              alert(`Pasta ${this.folderName} created, enjoy.`);
+              this.logs.push(new Log(`Created Directory ${this.folderName}`, JSON.stringify(res)));
+
+            }).catch(err => {
+              this.logs.push(new Log(`Error creating folder ${this.folderName}`, JSON.stringify(err)));
+              alert(`Error creating folder ${this.folderName}: ${JSON.stringify(err)}`);
+            });
+        });
+    }
   }
-
 }
